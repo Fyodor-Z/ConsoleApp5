6dff49cc3581cc072619491ae933548095158430
fyodor <fyodor.zakharov@gmail.com>
2021-07-11 23:28:09 +0300

    add comment

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ConsoleApp5
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person { Name = "Tom", Age = 35 };
+            string json = JsonSerializer.Serialize<Person>(tom);
+            Console.WriteLine(json);
+            Person restoredPerson = JsonSerializer.Deserialize<Person>(json);
+            Console.WriteLine(restoredPerson.Name);
+
+            //branch1
+        }
+    }
+}
